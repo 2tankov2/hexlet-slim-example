2bc7e7b37d00b61050058860a0b7082b1e24748b
tankov <tan.kov@hotmail.com>
2020-01-05 17:24:35 +0300

    add page children & add handlers

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
+
 $container = new Container();
 $container->set('renderer', function () {
     // Параметром передается базовая директория в которой будут храниться шаблоны
@@ -16,13 +17,38 @@ $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
+    return $this->get('renderer')->render($response, 'index.phtml');
+});
+///////////////////////////////////////////////////////////////////////////////////////////////
+$app->get('/salute', function ($request, $response) {
     return $response->write('Welcome to Slim!');
 });
-
+///////////////////////////////////////////////////////////////////////////////////////////////
 $app->get('/users', function ($request, $response) {
     return $response->write('GET /users');
 });
 
+$app->get('/users/{id}', function ($request, $response, $args) {;
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации;
+    // $this доступен внутри анонимной функции благодаря http://php.net/manual/ru/closure.bindto.php;
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////
+$children = [
+['id' => 1, 'name' => 'Maxim', 'age' => 12],
+['id' => 2, 'name' => 'Lena', 'age' => 5],
+['id' => 3, 'name' => 'Roman', 'age' => 7],
+['id' => 4, 'name' => 'Jenya', 'age' => 10]
+];
+
+$app->get('/children', function ($request, $response) use ($children) {
+    $params = [
+        'children' => $children
+    ];
+    return $this->get('renderer')->render($response, 'children/child.phtml', $params);
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
@@ -31,19 +57,42 @@ $app->get('/courses/{id}', function ($request, $response, array $args) {
 $app->get('/courses/{courseId}/lessons/{id}', function ($request, $response, array $args) {
     $courseId = $args['courseId'];
     $id = $args['id'];
-    return $response->write("Course id: {$courseId}")
-      ->write("Lesson id: {$id}");
+    return $response->write("Course id: {$courseId}; ")
+        ->write("Lesson id: {$id}");
 });
 
-
-$app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
-    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
-    // $this доступен внутри анонимной функции благодаря http://php.net/manual/ru/closure.bindto.php
-    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
+var_dump($_SERVER);
 
 $app->post('/users', function ($request, $response) {
     return $response->withStatus(302);
 });
+
 $app->run();
+/*
+
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$useres = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
+$app->get('/users', function ($request, $response) use ($useres) {
+    $term = $request->getQueryParam('term');
+    $users = $useres;
+    if (isset($term)) {
+        $users = array_filter($users, function ($user) use ($term) {
+            return strpos($user, $term) !== false;
+        });
+    }
+    $params = [
+        'users' => $users,
+        'term' => $term
+    ];
+    return $this->get('renderer')->render($response, 'users/list.phtml', $params);
+});
+
+var_dump($_GET);
+
+*/
