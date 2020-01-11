16c914cd25946b62966a9be726f69412adf88b2c
tankov <tan.kov@hotmail.com>
2020-01-11 14:54:49 +0300

    add userList

--- public/index.php
+++ public/index.php
@@ -6,8 +6,14 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
+session_start();
 
 $container = new Container();
+
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+
 $container->set('renderer', function () {
     // Параметром передается базовая директория в которой будут храниться шаблоны
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
@@ -19,23 +25,23 @@ $app->addErrorMiddleware(true, true, true);
 $router = $app->getRouteCollector()->getRouteParser();
 
 $app->get('/', function ($request, $response) use ($router) {
-    
-    $router->urlFor('salute');
+
     $router->urlFor('main');
+    $router->urlFor('salute');
+    $router->urlFor('users');
+    $router->urlFor('user');
     $router->urlFor('names');
-    
     $router->urlFor('children');
-    
+
+    $messages = $this->get('flash')->getMessages();
+    print_r($messages);
+
     return $this->get('renderer')->render($response, 'index.phtml');
 })->setName('main');
 ///////////////////////////////////////////////////////////////////////////////////////////////
 $app->get('/salute', function ($request, $response) {
     return $response->write('Welcome to Slim!');
 })->setName('salute');
-///////////////////////////////////////////////////////////////////////////////////////////////
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
-});
 ///////////////////////////////////////////////////////////////////////////////////////////////
 $users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
 
@@ -87,7 +93,7 @@ $app->get('/courses/{courseId}/lessons/{id}', function ($request, $response, arr
     return $response->write("Course id: {$courseId}; ")
         ->write("Lesson id: {$id}");
 });
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 var_dump($_SERVER);
 
 $app->get('/users/new', function ($request, $response) {
@@ -98,20 +104,20 @@ $app->get('/users/new', function ($request, $response) {
     return $this->get('renderer')->render($response, "users/new.phtml", $params);
 })->setName('users/new');
 
-$app->get('/users/{name}', function ($request, $response, array $args) {
-    $name = $args['name'];
+$app->get('/users/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
     $lines = file('./fixtures/users.json');
-    
+
     foreach ($lines as $line) {
-        $userNames[] = json_decode($line, true);
+        $users[] = json_decode($line, true);
     }
     
-    $userName = collect($userNames)->firstWhere('name', $name);
-    if (!$userName) {
+    $user = collect($users)->firstWhere('id', $id);
+    if (!$user) {
         return $response->withStatus(404)->write('User not found');
     }
-    return $response->write(json_encode($userName));
-});
+    return $response->write(json_encode($user));
+})->setName('user');
 
 //$app->post('/users', function ($request, $response) {
 //    return $response->withStatus(302);
@@ -135,14 +141,27 @@ $app->post('/users', function ($request, $response) {
     if (empty($errors)) {
         fwrite($handle, json_encode($user));
         fclose($handle);
-        return $response->withHeader('Location', '/')
-            ->withStatus(302);
+        $this->get('flash')->addMessage('success', 'This is a message');
+        return $response->withStatus(302)->withHeader('Location', '/');
     }
-    $params = [
+        $params = [
         'user' => $user,
         'errors' => $errors
     ];
-    return $this->get('renderer')->render($response, "users/new.phtml", $params);
+        return $this->get('renderer')->render($response, "users/new.phtml", $params);
 });
+
+$app->get('/users', function ($request, $response) {
+    $lines = file('./fixtures/users.json');
+
+    foreach ($lines as $line) {
+        $users[] = json_decode($line, true);
+    }
+
+    $params = [
+        'users' => $users
+    ];
+    return $this->get('renderer')->render($response, "users/index.phtml", $params);
+})->setName('users');
 
 $app->run();
